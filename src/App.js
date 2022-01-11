1a9356baad8d5c160fd5d928d8f763bac1a063cd
Utkarshsgithub <utkarshcodes13@gmail.com>
2022-01-11 23:33:57 +0530

    project ready 100%

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import data from "./data";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let quantity = parseInt(count);
+
+    if (quantity <= 0) {
+      quantity = 1
+    }
+
+    setText(data.slice(0, quantity));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>
+        Tired of Boring <strong>Lorem, ipsum?</strong>
+      </h1>
+      <form onSubmit={handleSubmit} id="form">
+        <label htmlFor="quantity">
+          <strong>Paragraphs: </strong>
+        </label>
+        <input
+          name="quantity"
+          id="quantity"
+          type="number"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit" id="btn">
+          <strong>Generate</strong>
+        </button>
+      </form>
+      <article>
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
+      </article>
     </div>
   );
 }
